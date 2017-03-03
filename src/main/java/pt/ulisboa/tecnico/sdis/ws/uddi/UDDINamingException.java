30c09ca0c462407caae7ae8c2ba4417364d0550b
Miguel Pardal <miguel.pardal@tecnico.ulisboa.pt>
2017-03-03 10:11:57 +0000

    Add class comments.

--- src/main/java/pt/ulisboa/tecnico/sdis/ws/uddi/UDDINamingException.java
+++ src/main/java/pt/ulisboa/tecnico/sdis/ws/uddi/UDDINamingException.java
@@ -2,6 +2,12 @@ package pt.ulisboa.tecnico.sdis.ws.uddi;
 
 import javax.xml.registry.JAXRException;
 
+/**
+ * Class that represents a UDDI naming exception. This class extends
+ * JAXRException and is intended to provide more meaningful exception messages.
+ * 
+ * @author Miguel Pardal
+ */
 public class UDDINamingException extends JAXRException {
 
 	private static final long serialVersionUID = 1L;
